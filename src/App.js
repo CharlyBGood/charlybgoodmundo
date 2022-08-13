aa5129e6622c9629ec9a653edcbe4f92e521444a
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-08-13 00:43:24 -0300

    add navbar and footer templates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Charly BGood Mundo</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../stylesheets/Navbar.css";
+import Logo from "../img/headerlogo.png";
+
+function Navbar() {
+    return(
+        <nav className="topnav" id="nav-bar">
+        <a className="nav-link_1" href="/">
+            <img className="header-logo" src={Logo} alt="charly bgood logo" />
+        </a>
+        <a className="nav-link_2" href="./pages/contact.html">contact</a>
+    </nav>
+    );
+}
+
+export default Navbar;
