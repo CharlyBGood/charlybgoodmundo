1109aed3c06198e6dcf036ebd358797008f724db
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-08-17 23:16:11 -0300

    add icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,28 +4,28 @@ import "../stylesheets/Footer.css";
 function Footer() {
   return (
     <footer className="smedia">
-      <a href="https://bit.ly/BGoodYouTube" target="_blank" rel="noreferrer">
-        y
-      </a>
       <a href="https://github.com/CharlyBGood" target="_blank" rel="noreferrer">
-        g
+        <i class="fa-brands fa-github"></i>
       </a>
+      <a href="https://bit.ly/BGoodYouTube" target="_blank" rel="noreferrer">
+        <i class="fab fa-youtube"></i>
+      </a>      
       <a href="https://vimeo.com/charlybgood" target="_blank" rel="noreferrer">
-        v
+        <i class="fab fa-vimeo-v"></i>
       </a>
       <a
         href="https://www.instagram.com/charlsbgood/"
         target="_blank"
         rel="noreferrer"
       >
-        i
+        <i class="fab fa-instagram"></i>
       </a>
       <a
         href="https://twitter.com/CharlybGoodM"
         target="_blank"
         rel="noreferrer"
       >
-        t
+        <i class="fab fa-twitter"></i>
       </a>
     </footer>
   );
