9160e4b6e5708df254aa8a0d3410f6f81cece76d
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-11-14 11:34:01 -0300

    add projects on devPortfolio section

--- src/components/ReactProjects.js
+++ src/components/ReactProjects.js
@@ -4,12 +4,20 @@ function ReactProjects() {
   return (
     <div className="decoration_words">
       <a
-      className="item item_1"
+        className="item item_1"
         href="https://classic-todo-app.vercel.app/"
         target="_blank"
         rel="noreferrer"
       >
         #NoStateNodeTodoApp
+      </a>
+      <a      
+        className="item item_2"
+        href="https://test-app-mocha-three.vercel.app/"        
+        target="_blank"
+        rel="noreferrer"
+      >
+        #TestimoniesMockup
       </a>
     </div>
   );
